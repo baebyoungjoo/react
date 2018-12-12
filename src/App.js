27fe7ba8ea10054edc78ec4911c926da7cd1a08f
bj.bae <ulsanbbj@gmail.com>
2018-12-12 22:26:14 +0900

    first react header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import FirstHeader from './components/FirstHeader'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <FirstHeader></FirstHeader>
       </div>
     );
   }
---
+++ src/components/FirstHeader.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FirstHeader extends Component {
+    render () {
+        return (
+            <div>
+                <h1>First React Example!</h1>
+            </div>
+        )
+    }
+}
+
+export default FirstHeader;
