86f8e4edf3e92f1c600d860ec020e659e69119bc
bjbae <bj.bae@usafe.co.kr>
2018-12-13 17:45:10 +0900

    onclick event add
    todo list.
    3 or more..

--- src/components/Td.js
+++ src/components/Td.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 class Td extends React.Component {
 
+    state = {
+        isActive: false
+    }
+
     getToday = () => {
         return new Date().getFullYear() + '.' + (new Date().getMonth() + 1) + '.' + new Date().getDate() + '.'
     }
@@ -17,25 +21,45 @@ class Td extends React.Component {
             ? lastClassName = lastClassName + 'calendar_today' 
             : lastClassName = lastClassName + '';
 
+        if (this.state.isActive) lastClassName = lastClassName + ' calendar_select';
+
         return lastClassName
+    }
+
+    handleOnClick = () => {
+        const currentState = this.state.isActive;
+        this.setState({
+            isActive: !currentState
+        })
     }
 
     render() {
         const { thisDate, innerIdx, value } = this.props
+        const { isActive } = this.state
         return (
             <td 
                 className={ this.checkClassName() }
                 data-date={ thisDate }
                 key={ innerIdx }
+                onClick={ this.handleOnClick }
             >
             { value }
             <br/>
             
             { 
-                thisDate === this.getToday() 
-                    ? <span>오늘</span>
-                    : null
+                thisDate === this.getToday() && isActive === true
+                    ? <span>select</span>
+                    : thisDate === this.getToday() && isActive !== true 
+                        ? <span>오늘</span>
+                            : isActive === true
+                                ? <span>select</span>
+                                : null
             }
+            {/* TODO
+                1. 날짜 선택하면 입력창 수정 가능하게
+                2. 연속 날짜 선택 시 범위 표시
+                3. 날짜 선택 후, 월 변경해도 선택한건 남아있게..
+            */}
             </td>
         )
     }
